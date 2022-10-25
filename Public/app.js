b793373b967f9023304315d9ee926a1c8a0c7b8d
Fabrício Hiury <fabhiury@gmail.com>
2022-10-25 16:53:28 -0300

    Implementation of function delete

--- Public/app.js
+++ Public/app.js
@@ -5,18 +5,30 @@ async function loadProducts(){
 
     const list = document.getElementById('list-products')
 
-    products.array.forEach(product => {
+    products.forEach(product => {
         const item = document.createElement('li')
         item.innerText = product.name
-        item.innerText = product.price 
 
         list.appendChild(item)
     });
     
 }
+
+function manipulateForm() {
+    const form_product = document.getElementById('form-product')
+    const input_name = document.getElementById('name')
+
+    form_product.onsubmit = (event)=> {
+        event.preventDefault()
+        const name_product = input_name.value
+        alert(`submit chamado ${name_product}`)
+    }
+}
+
 function app() {
     console.log("API started")
     loadProducts()
+    manipulateForm()
 }
 
 app()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from unicodedata import name
+from turtle import position
+from uuid import uuid4
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List 
+from typing import List, Optional 
 from pydantic import BaseModel
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -39,9 +41,10 @@ async def remove():
 @app.get("/saudacao/{name}")
 async def saudacao(name: str):
     text = f'Olá {name}, é um prazer vê-lo novamente!'
-    return text
+    return text 
 
 class Products(BaseModel):
+    id: Optional[str]
     name: str
     price: float
 
@@ -51,11 +54,32 @@ async def products(product: Products):
 
 bank: List[Products] = []
 
-@app.get('/products')
+@app.get("/products")
 def list_products():
     return bank
 
-@app.post('/products')
+@app.get("/products/{product_id}")
+def get_products(product_id: str):
+    for product in bank:
+        if product.id == product_id:
+            return product
+    return {'error': 'Product not found'} 
+
+@app.delete("/products/{product_id}")
+def delete_products(product_id: str):
+    position = -1
+    for index, product in enumerate(bank):
+        if product.id == product_id:
+            position = index
+            break
+    if position  != -1:
+        bank.pop(position)
+        return{'mensagem': 'Product deleted'}
+    else:
+        return {'mensagem' : f'Product not found with id {id}'}
+
+@app.post("/products")
 def create_products(product: Products):
+    product.id = str(uuid4())
     bank.append(product)
     return None
